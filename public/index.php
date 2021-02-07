22c652d0540db11dee7c846d3de82a45926d7f25
Shelton Gayles <shelton@sheltons-mbp.lan>
2021-02-07 14:37:49 -0800

    add subroute names like admin to route

--- public/index.php
+++ public/index.php
@@ -31,6 +31,7 @@ $router = new Core\Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 
 /*
 // Display the routing table
